02d49beaf8af76ecf84c6d39947ef696bdf76b70
kimhyunna <huuu0890@naver.com>
2022-01-14 15:05:42 +0900

    Update IntroActivity.kt
    주석추가

--- HairshopApp/app/src/main/java/com/cookandroid/fill/IntroActivity.kt
+++ HairshopApp/app/src/main/java/com/cookandroid/fill/IntroActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+//kimhyunna 인트로화면 보여주기 01.14
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
