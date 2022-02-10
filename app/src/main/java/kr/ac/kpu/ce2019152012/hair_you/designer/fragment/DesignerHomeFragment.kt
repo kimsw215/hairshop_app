2b2a976aa51ba58d32d6d1cce92fb52b49f352f4
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-02-11 01:01:31 +0900

    Add HA's code

--- app/src/main/java/kr/ac/kpu/ce2019152012/hair_you/designer/fragment/DesignerHomeFragment.kt
+++ app/src/main/java/kr/ac/kpu/ce2019152012/hair_you/designer/fragment/DesignerHomeFragment.kt
@@ -1,23 +1,34 @@
-package kr.ac.kpu.ce2019152012.hair_you.fragment
+package kr.ac.kpu.ce2019152012.hair_you.designer.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import kr.ac.kpu.ce2019152012.hair_you.R
 
-class DesignerSettingFragment : Fragment() {
+class DesignerHomeFragment : Fragment() {
+    lateinit var reserStatus : TextView
+    lateinit var reserManagement : TextView
+    lateinit var back_icon : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_designer_setting, container, false)
+        val view=inflater.inflate(R.layout.fragment_designer_home, container, false)
+
+
+
+        return view
     }
 
 
