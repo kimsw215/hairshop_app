e517c5afa396084a4a1e344d50f386a02fe72aef
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2022-03-15 16:39:02 +0900

    kakao_login

--- app/src/main/java/kr/ac/kpu/ce2019152012/hair_you/GlobalApplication.kt
+++ app/src/main/java/kr/ac/kpu/ce2019152012/hair_you/GlobalApplication.kt
@@ -4,11 +4,14 @@ import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 
 class GlobalApplication : Application() {
+    /*companion object{
+        var appContext : Context? = null
+    }*/
+
     override fun onCreate() {
         super.onCreate()
-        var instance = this
+        /*appContext = this*/
         // 다른 초기화 코드들
-
 
         // Kakao SDK 초기화
         KakaoSdk.init(this, "7d73ad51a6a4a5808b30eb48286e214c")
--- app/src/main/java/kr/ac/kpu/ce2019152012/hair_you/LoginActivity.kt
+++ app/src/main/java/kr/ac/kpu/ce2019152012/hair_you/LoginActivity.kt
@@ -1,11 +1,104 @@
 package kr.ac.kpu.ce2019152012.hair_you
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.AuthErrorCause
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.usermgmt.LoginButton
+
+import kr.ac.kpu.ce2019152012.hair_you.databinding.ActivityLoginBinding
+import kr.ac.kpu.ce2019152012.hair_you.user.UserMainActivity
+import kr.ac.kpu.ce2019152012.hair_you.user.UserMainActivity.Companion.TAG
+import kr.ac.kpu.ce2019152012.hair_you.user.fragment.HomeFragment
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var bingding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        bingding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(bingding.root)
+
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                Toast.makeText(this, "토근 정보 보기 실패", Toast.LENGTH_SHORT).show()
+            } else if (tokenInfo != null) {
+                Toast.makeText(this, "토근 정보 보기 성공", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, UserMainActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
+            }
+        }
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                when {
+                    error.toString() == AuthErrorCause.AccessDenied.toString() -> {
+                        Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidClient.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
+                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
+                        Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidScope.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Misconfigured.toString() -> {
+                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    error.toString() == AuthErrorCause.ServerError.toString() -> {
+                        Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Unauthorized.toString() -> {
+                        Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> { // Unknown
+                        Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else if (token != null) {
+                Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, UserMainActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
+            }
+        }
+
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        bingding.kakaologin.setOnClickListener {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리(ex: 뒤로가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        //카카오톡에 연결된 카카오계정이 없는 경우, 카카오 계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
+
+
     }
 }
